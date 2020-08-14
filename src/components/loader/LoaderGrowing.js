dc37c8ccfed4de7128c3e13edf1209c51903dbdb
Haekal Zidni Barkan <hzbarkan@gmail.com>
2020-08-14 23:57:24 +0700

    Updates

--- src/components/loader/LoaderGrowing.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Grow } from './styles'
-
-const LoaderGrowing = ({ color }) => (
-  <Grow className='spinner-grow text-danger' color={color} />
-)
-
-export default LoaderGrowing
